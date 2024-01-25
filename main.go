9607e6d5ddb7bf774e8c8acfea1175effdd241ea
Camilo Mezú Mina <camilo272001@gmail.com>
2024-01-24 23:55:14 -0500

    explore all files and print the content to json

--- main.go
+++ main.go
@@ -1,28 +1,71 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	S "strings"
 )
 
-func main() {
-	dir, err := os.Getwd()
+type Email struct {
+	Headers map[string]string `json:"headers"`
+	Body    string            `json:"body"`
+}
 
+func explore_file(path string) {
+	email_headers := [15]string{"Message-ID:", "Date:", "From:", "To:", "Subject", "Mime-Version:", "Contente-Type:", "Content-Transfer-Encoding:", "X-From:", "X-To:", "X-cc:", "X-bcc:", "X-Folder:", "X-Origin:", "X-FileName:"}
+	temp := email_headers[:]
+	file, err := os.Open(path)
 	if err != nil {
-		fmt.Println("Error getting current working directory:", err)
-		return
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var body string
+	var storeContent bool
+	headers := make(map[string]string)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		for i, header := range email_headers {
+			if S.HasPrefix(line, header) {
+				fmt.Println(line)
+				temp = append(temp[:i], temp[i+1:]...)
+				if S.HasPrefix(line, "X-FileName:") {
+					storeContent = true
+				} else {
+					storeContent = false
+				}
+				headers[header] = line[len(header):]
+				break
+			}
+		}
+		if storeContent {
+			body += line + "\n"
+		}
 	}
 
-	fmt.Println(dir)
-	folders, err := os.ReadDir(dir + "/enron_mail_20110402/maildir")
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
 
-	fmt.Println()
+	email := Email{
+		Headers: headers,
+		Body:    body,
+	}
+
+	jsonData, err := json.Marshal(email)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, file := range folders {
-		fmt.Println(file.Name())
-	}
+	fmt.Println(string(jsonData))
+}
+
+func main() {
+	//explore_dir()
+	explore_file("enron_mail_20110402/maildir/brawner-s/all_documents/2.")
 }
